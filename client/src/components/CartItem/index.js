1f84a869bca27874b679366ed01f3022a5619ae8
Duncan Byrne <duncanbyrne@Duncans-MacBook-Pro.local>
2021-09-27 22:24:38 -0400

    removed global state in utils edited the home page, details, and resolvers

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,44 +1,43 @@
 import React from 'react';
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
-import { idbPromise } from '../../utils/helpers';
 import { useDispatch, useSelector } from 'react-redux';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
-
-  const state = useSelector((state) => {
-    return state
-  });
-  const dispatch = useDispatch();
+      useSelector((state) => {
+      return state
+    });
+    const dispatch = useDispatch();
 
     const removeFromCart = item => {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id
+      });
+      idbPromise('cart', 'delete', { ...item });
+    };
+    
+    const onChange = (e) => {
+      const value = e.target.value;
+    
+      if (value === '0') {
         dispatch({
           type: REMOVE_FROM_CART,
           _id: item._id
         });
-        idbPromise('cart', 'delete', { ...item });
-      };
-
-
-    const onChange = (e) => {
-        const value = e.target.value;
       
-        if (value === '0') {
-          dispatch({
-            type: REMOVE_FROM_CART,
-            _id: item._id
-          });
+        idbPromise('cart', 'delete', { ...item });
+      } else {
+        dispatch({
+          type: UPDATE_CART_QUANTITY,
+          _id: item._id,
+          purchaseQuantity: parseInt(value)
+        });
+      
+        idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+      }
 
-          idbPromise('cart', 'delete', {...item})
-        } else {
-          dispatch({
-            type: UPDATE_CART_QUANTITY,
-            _id: item._id,
-            purchaseQuantity: parseInt(value)
-          });
-
-          idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value)})
-        }
-      };
+    };
 
 
   return (
@@ -59,13 +58,13 @@ const CartItem = ({ item }) => {
             value={item.purchaseQuantity}
             onChange={onChange}
           />
-            <span
+          <span
             role="img"
             aria-label="trash"
             onClick={() => removeFromCart(item)}
-            >
+          >
             🗑️
-            </span>
+          </span>
         </div>
       </div>
     </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,24 @@
-import React, { useState } from "react";
+import React , { useState }from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from '../components/Cart';
+
+
+
+// const Home = () => {
+//   const [currentCategory, setCategory] = useState("");
+
+  
+//   return (
+//     <div className="container">
+//       <CategoryMenu setCategory={setCategory} />
+//       <ProductList currentCategory={currentCategory} />
+//       <Cart />
+//     </div>
+//   );
+
+ 
+// };
 
 
 const Home = () => {
